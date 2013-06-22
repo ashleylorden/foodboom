2412eb8b749e660011e88ffcf21412a1f5feb4a2
Ashley Lorden <ashley.f.lorden@gmail.com>
2013-06-22 12:12:53 -0700

    restaurant class with pertinent information we can get through Yelp API

--- Restaurant.py
+++ Restaurant.py
@@ -1,2 +1,15 @@
 class Restaurant:
-	
+    yelp_id = None
+    name = None
+    mobile_url = None
+    categories = None
+    rating = None
+    neighborhoods = None
+
+    def __init__(self, id, name, mobile_url, categories, rating, neighborhoods):
+        self.yelp_id = id
+        self.name = name
+        self.mobile_url = mobile_url
+        self.categories = categories
+        self.rating = rating
+        self.neighborhoods = neighborhoods
