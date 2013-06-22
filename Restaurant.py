5a97540055d1fa3a54a4c3bd592228dbb89624f9
Ashley Lorden <ashley.f.lorden@gmail.com>
2013-06-22 13:02:10 -0700

    added method for loading restaurant attributes from Yelp dump

--- Restaurant.py
+++ Restaurant.py
@@ -6,10 +6,26 @@ class Restaurant:
     rating = None
     neighborhoods = None
 
-    def __init__(self, id, name, mobile_url, categories, rating, neighborhoods):
+    def __init__(self, id=None, name=None, mobile_url=None, categories=None,
+                 rating=None, neighborhoods=None):
         self.yelp_id = id
         self.name = name
         self.mobile_url = mobile_url
         self.categories = categories
         self.rating = rating
         self.neighborhoods = neighborhoods
+
+    def load_data(self, dictionary):
+        if "id" in dictionary:
+            self.yelp_id = dictionary["id"]
+        if "name" in dictionary:
+            self.name = dictionary["name"]
+        if "mobile_url" in dictionary:
+            self.mobile_url = dictionary["mobile_url"]
+        if "categories" in dictionary:
+            self.categories = dictionary["categories"]
+        if "rating" in dictionary:
+            self.rating = dictionary["rating"]
+        if "location" in dictionary:
+            if "neighborhoods" in dictionary["location"]:
+                self.neighborhoods = dictionary["location"]["neighborhoods"]
