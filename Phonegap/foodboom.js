5c6fde60caafc431abe2caf8f72a8045b2203a78
Diego Lorden <djlorden@gmail.com>
2013-06-23 10:07:36 -0700

    UI doing stuff

--- Phonegap/foodboom.js
+++ Phonegap/foodboom.js
@@ -4,38 +4,56 @@ function search(term){
         function(data){ 
             $('#search').empty();
             var restaurants = jQuery.parseJSON(data);
-            console.log(restaurants);
-
             for (r in restaurants) {
-                $('#search').append('<li data-theme="c">' + restaurants[r].name + '</li>');
+                $('#search').append(
+                    '<li data-theme="c">' + 
+                        '<a href="#" class="search-result" id="' + restaurants[r].yelp_id + '">' +
+                        restaurants[r].name + ' <span class="address">(' + restaurants[r].address + ')</span>'+ 
+                        '</a>' +
+                    '</li>'
+                );
             }
             $('ul').listview('refresh');
         });
 }
 
 function show_similar(yelp_id) {
-   $.get("http://foodboom.herokuapp.com/similar/" + yelp_id, function(data) {
-       restaurants = jQuery.parseJSON(data);
+    
+    $.get("http://foodboom.herokuapp.com/similar/" + yelp_id, function(data) {
+        restaurants = jQuery.parseJSON(data);
+        $('#search').empty();
+        for (var i=0; i<3; i++) {
+            $("#search").append(
+                '<li data-theme="b">' +
+                    '<a href="' + restaurants[i].mobile_url + 
+                        '" data-transition="slide">' + 
+                        restaurants[i].name + ' ' +
+                        ' - ' + restaurants[i].rating + ' stars - ' +
+                        Math.round(restaurants[i].distance) + ' mts away' +
+                    '</a>' +
+                '</li>'
+            );
+            $('ul').listview('refresh');
+       }
    });
-   $("#results_list").find('*').remove();
-   for (var i=0; i<3; i++) {
-       $("#results_list").append("<li data-theme='c'><a href='" + restaurants[i].mobile_url + "'
-                                  data-transition='slide'>" + restaurants[i].name + "</a></li>");
-   }
 }
-6:40 PM
- 
 
 
+var to;
+var term;
 $(document).ready(function(){
-    var mutex = 0;
     $('#search').parent().find('input').val('');
     $('#search').parent().find('input').keyup(function(){
-        if($(this).val().length > 2 && mutex == 0){
-            mutex = 1;
-            search($(this).val());
-            mutex = 0;
+        if($(this).val().length > 2 && term != $(this).val()){
+            window.clearTimeout(to);
+            term = $(this).val();
+            to = setTimeout(function(){ search(term) }, 700);
         }
+    });
+    $('body').on('click', 'a.search-result', function(){
+        $('#search').parent().find('input').val($(this).text());
+        // $('#search').parent().find('input').attr('readonly', 'readonly');
+        show_similar($(this).attr('id'));
     });
     
 });
